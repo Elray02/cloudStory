785e82c840e646e6fa9273f6d6d1a55d23bb6ead
Alberto <alberto.massa@ymail.com>
2017-06-05 22:43:06 +0200

    🅰🎨 Warp text on canvas

--- src/main.js
+++ src/main.js
@@ -4,13 +4,24 @@ import Story from 'storyGenerator.js'
 import Letter from 'Letter.js'
 import voce from './voice'
 
-function buildLetters(p, textToIterate) {
-  let spacingLetter = 10
-  const newLettere = []
-  for (let i = 0; i < textToIterate.length; i++) {
-    spacingLetter += 13
-    newLettere[i] = new Letter(p, spacingLetter, window.innerHeight / 2, textToIterate.charAt(i))
-  }
+const startX = 10
+const startY = window.innerHeight / 2
+const letterWidth = 13
+const letterHeight = 15
+
+function buildLetters(p, text) {
+  let x = startX
+  let y = startY
+  // string to array: https://stackoverflow.com/a/38066567/433685
+  const newLettere = Array.from(text).map((char, i) => {
+    if (x > window.innerWidth - startX) {
+      x = startX
+      y += letterHeight
+    }
+    const l = new Letter(p, x, y, char)
+    x += letterWidth
+    return l
+  })
   return newLettere
 }
 
@@ -48,6 +59,7 @@ const s = function (p) {
   function newStory(jsonInput) {
     var storyFactory = new Story(jsonInput.main.animal, jsonInput.main.umor)
     const story = storyFactory.createStory()
+    console.log(story)
     lettere = buildLetters(p, story)
     voice.speak(story)
   }
